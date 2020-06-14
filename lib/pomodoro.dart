262dbb4c37bfd41ba7860fbed38bffb587768e8a
Gunawan Lie <gunawanlie.toedjoeh@gmail.com>
2020-06-15 00:56:57 +0800

    timer animation created

--- lib/pomodoro.dart
+++ lib/pomodoro.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'task.dart';
+import 'tasks.dart';
+import 'task_type.dart';
+
 class Pomodoro extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -8,86 +12,80 @@ class Pomodoro extends StatefulWidget {
   }
 }
 
-class _PomodoroState extends State<Pomodoro> {
+class _PomodoroState extends State<Pomodoro> with SingleTickerProviderStateMixin {
 
-  List<Widget> _progressIndicators = [];
-
-  int taskIndex = 0;
-
-  LinearProgressIndicator _setupProgressIndicator(int index) {
-    Color backgroundColor = Color(0x660000FF);
-    Color valueColor = Color(0xFF0000FF);
-
-    if (index % 2 == 0) {
-      backgroundColor = Color(0x66FF0000);
-      valueColor = Color(0xFFFF0000);
-    } else if (index < 7) {
-      backgroundColor = Color(0x66228B22);
-      valueColor = Color(0xFF228B22);
-    }
-
-    return LinearProgressIndicator(
-      backgroundColor: backgroundColor,
-      value: 0.5,
-      valueColor: AlwaysStoppedAnimation<Color>(valueColor),
-    );
-  }
-
-  FlatButton _playButton = FlatButton(
-    child: Row(
-      children: <Widget>[
-        Icon(Icons.play_arrow),
-        Text("Start"),
-      ],
-    ),
-    color: Colors.blue,
-    onPressed: () {
-
-    },
-  );
-
-  FlatButton _nextButton = FlatButton(
-    child: Row(
-      children: <Widget>[
-        Text("Skip"),
-        Icon(Icons.skip_next),
-      ],
-    ),
-    color: Colors.blue,
-    onPressed: () {
-
-    },
-  );
-
-  List<Widget> _buttons = [];
+  Tasks _pomodoro = Tasks(25, 5, 15, 4, 1);
+  AnimationController _animationController;
+  String _pomodoroTimeLabel = "23:59";
 
   @override
   void initState() {
     super.initState();
-    _setupProgressIndicators();
-    _setupButtons();
+    _setupAnimationController();
+    _updatePomodoroTimerLabel();
   }
 
-  void _setupProgressIndicators() {
-    for (int i=0; i<8; i++) {
-      _progressIndicators.add(Expanded(
+  List<Widget> _progressIndicators() {
+    List<Widget> _indicators = [];
+
+    for (int i=0; i<_pomodoro.count(); i++) {
+      double value = (i < _pomodoro.currentTaskIndex() ? 1.0 : (i > _pomodoro.currentTaskIndex() ? 0.0 : _animationController.value));
+
+      _indicators.add(Expanded(
         child: Container(
-          child: _setupProgressIndicator(i),
+          child: _progressIndicator(_pomodoro.task(i), value),
           padding: EdgeInsets.symmetric(horizontal: 1.0),
         ),
       ));
     }
+
+    return _indicators;
   }
 
-  void _setupButtons() {
-    _buttons.add(Container(
-      child: _playButton,
-      padding: EdgeInsets.symmetric(horizontal: 5.0),
-    ));
-    _buttons.add(Container(
-      child: _nextButton,
-      padding: EdgeInsets.symmetric(horizontal: 5.0),
-    ));
+  LinearProgressIndicator _progressIndicator(Task task, double value) {
+    return LinearProgressIndicator(
+      backgroundColor: task.type.backgroundColor,
+      value: value,
+      valueColor: AlwaysStoppedAnimation<Color>(task.type.foregroundColor),
+    );
+  }
+
+  void _setupAnimationController() {
+    _animationController = AnimationController(
+      duration: Duration(minutes: _pomodoro.currentTaskDuration()),
+      vsync: this,
+    );
+
+    _animationController.reset();
+    _animationController.addListener(() {
+      if (_animationController.isCompleted) {
+        _refreshAnimatioNController();
+      }
+      _updatePomodoroTimerLabel();
+      setState(() {});
+    });
+  }
+
+  void _updatePomodoroTimerLabel() {
+    int duration = _pomodoro.currentTaskDuration() * 60;
+    double progress = _animationController.upperBound - _animationController.value;
+    int left = (progress * duration).floor();
+    int minutes = (left / 60).floor();
+    int seconds = left % 60;
+    _pomodoroTimeLabel = "${minutes}:${seconds.toString().padLeft(2, '0')}";
+  }
+
+  void _refreshAnimatioNController() {
+    _animationController.stop();
+    _pomodoro.next();
+    _animationController.duration = Duration(minutes: _pomodoro.currentTaskDuration());
+    _animationController.value = _animationController.lowerBound;
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -100,18 +98,73 @@ class _PomodoroState extends State<Pomodoro> {
             child: Column(
               children: <Widget>[
                 Text(
-                  "23:59",
+                  _pomodoroTimeLabel,
                   style: TextStyle(
                     fontFamily: 'Teko',
                     fontSize: 150,
                   ),
                 ),
                 Row(
-                  children: _progressIndicators,
+                  children: _progressIndicators(),
                   mainAxisAlignment: MainAxisAlignment.center,
                 ),
                 Row(
-                  children: _buttons,
+                  children: <Widget>[
+                    Visibility(
+                      child: Container(
+                        child: FlatButton(
+                          child: Row(
+                            children: <Widget>[
+                              Icon(Icons.play_arrow),
+                              Text("Start"),
+                            ],
+                          ),
+                          color: Colors.blue,
+                          onPressed: () {
+                            _animationController.forward();
+                            setState(() {});
+                          },
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: 5.0),
+                      ),
+                      visible: !_animationController.isAnimating,
+                    ),
+                    Visibility(
+                      child: Container(
+                        child: FlatButton(
+                          child: Row(
+                            children: <Widget>[
+                              Icon(Icons.pause),
+                              Text("Pause"),
+                            ],
+                          ),
+                          color: Colors.blue,
+                          onPressed: () {
+                            _animationController.stop();
+                            setState(() {});
+                          },
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: 5.0),
+                      ),
+                      visible: _animationController.isAnimating,
+                    ),
+                    Container(
+                      child: FlatButton(
+                        child: Row(
+                          children: <Widget>[
+                            Text("Skip"),
+                            Icon(Icons.skip_next),
+                          ],
+                        ),
+                        color: Colors.blue,
+                        onPressed: () {
+                          _refreshAnimatioNController();
+                          setState(() {});
+                        },
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 5.0),
+                    ),
+                  ],
                   mainAxisAlignment: MainAxisAlignment.center,
                 ),
               ],
--- lib/tasks.dart
+++ lib/tasks.dart
@@ -3,24 +3,44 @@ import 'task_type.dart';
 
 class Tasks {
 
-  int index;
-  List<Task> list = [];
+  int _index;
+  List<Task> _list = [];
 
   Tasks(int taskDuration, int shortRestDuration, int longRestDuration, int sets, int cycle) {
-    this.index = 0;
+    _index = 0;
     
     for (int c=0; c<cycle; c++) {
       for (int s=0; s<sets; s++) {
-        list.add(Task(taskDuration, TaskType.task));
+        _list.add(Task(taskDuration, TaskType.task));
         if (!_isLastSet(s, sets)) {
-          list.add(Task(shortRestDuration, TaskType.shortRest));
+          _list.add(Task(shortRestDuration, TaskType.shortRest));
         }
       }
-      list.add(Task(longRestDuration, TaskType.longRest));
+      _list.add(Task(longRestDuration, TaskType.longRest));
     }
   }
 
   bool _isLastSet(int s, int sets) {
     return s == sets-1;
+  }
+
+  int count() {
+    return _list.length;
+  }
+
+  int currentTaskIndex() {
+    return _index;
+  }
+
+  int currentTaskDuration() {
+    return _list[_index].duration;
+  }
+
+  void next() {
+    _index = (_index + 1) % _list.length;
+  }
+
+  Task task(int i) {
+    return _list[i];
   }
 }
